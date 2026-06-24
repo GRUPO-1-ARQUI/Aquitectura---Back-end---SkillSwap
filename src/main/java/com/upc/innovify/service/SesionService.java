3d84399ec177db17d33fe82974e0a84a2a583b28
catssist <ariana.th1411@gmail.com>
2026-06-24 14:58:10 -0500

    fix: fix SesionService-Ariana Isla

--- src/main/java/com/upc/innovify/service/SesionService.java
+++ src/main/java/com/upc/innovify/service/SesionService.java
@@ -1,37 +1,18 @@
 package com.upc.innovify.service;
 
 import com.upc.innovify.model.Sesion;
-import com.upc.innovify.repository.SesionRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-@RequiredArgsConstructor
-public class SesionService {
-    private final SesionRepository sesionRepository;
+public interface SesionService {
 
-    public List<Sesion> getAll() {
-        return sesionRepository.findAll();
-    }
+    List<Sesion> getAll();
 
-    public Optional<Sesion> getById(Integer id) {
-        return sesionRepository.findById(id);
-    }
+    Optional<Sesion> getById(Integer id);
 
-    public List<Sesion> getBySolicitud(Integer idSolicitud) {
-        return sesionRepository.findByIdSolicitud(idSolicitud);
-    }
+    List<Sesion> getBySolicitud(Integer idSolicitud);
 
-    public Sesion create(Sesion sesion) {
-        sesion.setEstado("programada");
-        return sesionRepository.save(sesion);
-    }
+    Sesion create(Sesion sesion);
 
-    public Sesion updateEstado(Integer id, String estado) {
-        Sesion sesion = sesionRepository.findById(id).orElseThrow();
-        sesion.setEstado(estado);
-        return sesionRepository.save(sesion);
-    }
+    Sesion updateEstado(Integer id, String estado);
 }
